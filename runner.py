3321e0b7414b36c9550f8b45a416b3920b8ca5f3
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-08-20 19:40:35 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 # Standard library modules
 import json
 import os

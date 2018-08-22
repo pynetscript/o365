3ca7ba0b2f4dacdb0a1f6c2255e69023172835f3
Aleks Lambreca <31127654+pynetscript@users.noreply.github.com>
2018-08-22 14:42:58 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,7 +3,7 @@
 ###############################################################################
 # Written by:           Aleks Lambreca
 # Creation date:        19/08/2018
-# Last modified date:   21/08/2018
+# Last modified date:   22/08/2018
 # Version:              v1.0
 ###############################################################################
 
@@ -13,6 +13,7 @@ import json
 import os
 import urllib.request
 import uuid
+import requests
 import time
 import datetime
 import signal
@@ -56,13 +57,31 @@ def slack_message(channel, username, icon_url, text):
     sc.api_call('chat.postMessage', channel=channel, username=username, icon_url=icon_url, text=text)
 
 
+def slack_upload(channels, initial_comment, title, filename, filetype, file):
+    """
+    Upload file to slack
+    """
+    class SlackBotsAPITokenNotFound(Exception):
+        pass
+    
+    BOTS_TOKEN = os.environ.get('BOTS_TOKEN')
+    if BOTS_TOKEN is None:
+        NOT_FOUND = 'BOTS_TOKEN was not found in environment variable "/etc/environment".'
+        COMMAND = 'Add token with BOTS_TOKEN="xoxb-0123456789a0-123456789b01-23456789c0123456789d0123" format.'
+        print('\n'
+              '{E1} {E2} \n'.format(E1=NOT_FOUND, E2=COMMAND))
+        raise SlackBotsAPITokenNotFound()
+
+    sc = SlackClient(BOTS_TOKEN)
+    sc.api_call('files.upload', channels=channels, initial_comment=initial_comment, title=title, filename=filename, filetype=filetype, file=file)
+
+    
 # slack image
 icon_url = "https://avatars.slack-edge.com/2018-08-14/416017134033_c12382bddd39e3823d99_48.jpg"
 
 
 # path where client ID and latest version number will be stored
 path = os.path.dirname(os.path.abspath(__file__))
-
 datapath = (path + '/clientrequestid_latestversion.txt')
 
 
@@ -113,7 +132,6 @@ if version['latest'] > latestVersion:
         if endpointSet['category'] in ('Optimize', 'Allow'):
             ips = endpointSet['ips'] if 'ips' in endpointSet else []
             category = endpointSet['category']
-            # IPv4 strings have dots while IPv6 strings have colons
             ip4s = [ip for ip in ips if '.' in ip]
             tcpPorts = endpointSet['tcpPorts'] if 'tcpPorts' in endpointSet else ''
             udpPorts = endpointSet['udpPorts'] if 'udpPorts' in endpointSet else ''
@@ -130,20 +148,41 @@ if version['latest'] > latestVersion:
     with open(datapath, 'r') as fin:
         clientRequestId = fin.readline().strip()
 
+    path_o365_ipv4 = os.path.dirname(os.path.abspath(__file__))
+    data_o365_ipv4 = (path_o365_ipv4 + '/o365_ipv4.csv')
+    with open(data_o365_ipv4, 'w') as data_o365_ipv4_out:
+        data_o365_ipv4_out.write(o365_ipv4)
 
-    text = ('New version of Office 365 worldwide commercial service instance endpoints detected at\n' +
+    path_o365_url = os.path.dirname(os.path.abspath(__file__))
+    data_o365_url = (path_o365_url + '/o365_url.csv')
+    with open(data_o365_url, 'w') as data_o365_url_out:
+        data_o365_url_out.write(o365_urls)
+
+
+    text0 = ('*New version of Office 365 worldwide commercial service instance endpoints detected at:*\n' +
             'https://endpoints.office.com/version/Worldwide?clientrequestid=' + clientRequestId + '\n\n'
-            'Current version: ' + latestVersion + ' (' + datapath + ')' + '\n'
-            'Latest version: ' + latest_version + '\n\n\n'
-            'IPv4 Address Ranges for Firewall \n\n' +
-            o365_ipv4 + '\n\n\n'
-            'URLs for Firewall \n\n' +
-            o365_urls + '\n\n\n'
-            'IPv4 Address Ranges & URLs TCP/UDP ports \n\n' +
-            'TCP: ' + o365_tcp + '\n' +
-            'UDP: ' + o365_udp_fix)
+            'Current version: ' + '`' + latestVersion + '`' + ' (' + datapath + ')' + '\n'
+            'Latest version: ' + '`' + latest_version + '`' + '\n\n\n')
+    slack_message('#o365', 'mr-robot', icon_url, text0)
 
-    slack_message('#o365', 'mr-robot', icon_url, text)
+    slack_upload('#o365',
+                 '*IPv4 Address Ranges for Firewall*',
+                 'o365_ipv4.csv',
+                 'o365_ipv4.csv',
+                 'csv',
+                 (data_o365_ipv4, open(data_o365_ipv4, 'rb'), 'csv'))
+
+    slack_upload('#o365',
+                 '*URLs for Firewall*',
+                 'o365_url.csv',
+                 'o365_url.csv',
+                 'csv',
+                 (data_o365_url, open(data_o365_url, 'rb'), 'csv'))
+
+    text1 = ('*IPv4 Address Ranges & URLs TCP/UDP ports* \n\n' +
+             'TCP: ' + '```' + o365_tcp + '```' + '\n' +
+             'UDP: ' + '```' + o365_udp_fix + '```')
+    slack_message('#o365', 'mr-robot', icon_url, text1)
 
 
 else:
@@ -151,8 +190,8 @@ else:
     current_time = current_timestamp.strftime('%d/%m/%Y %H:%M:%S')
     print(current_time, '- Office 365 worldwide commercial service instance endpoints are up-to-date.')
 
-    text = ('Office 365 worldwide commercial service instance endpoints are up-to-date.'
-            '\nCurrent version: ' + latestVersion +
-            '\nLatest version: ' + latest_version)
+    text = ('*Office 365 worldwide commercial service instance endpoints are up-to-date.*'
+            '\nCurrent version: ' + '`' + latestVersion + '`' +
+            '\nLatest version: ' + '`' + latest_version + '`' )
 
     slack_message('#o365', 'mr-robot', icon_url, text)
